4f48507aa2cf99f4a06ee5a67d0bf0cb21428244
mes32 <stockman.mike@gmail.com>
2018-07-28 16:14:28 -0500

    feature: Initialize gradle build tool.

---
+++ src/main/java/mindvision/Main.java
@@ -0,0 +1,15 @@
+/*
+    Main.java
+
+    Launcher class for MindVision
+ */
+
+package mindvision;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("in Main.java");
+    }
+}
